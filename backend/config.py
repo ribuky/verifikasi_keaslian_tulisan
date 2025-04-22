16c7976b150e9c22d6c538a4f603669930f54c40
Muhammad Rivky Ananda <rivkyananda33@gmail.com>
2025-04-23 04:56:09 +0700

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -2,9 +2,11 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL", "mysql+pymysql://root:@localhost/verifikasi_tugas"
-    )
+    # SQLALCHEMY_DATABASE_URI = os.getenv(
+    #     "DATABASE_URL", "mysql+pymysql://root:@localhost/verifikasi_tugas"
+    # )
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://verifikasi_user:passwordku123@localhost/verifikasi_tugas"
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwtsecret")
 
